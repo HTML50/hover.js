55b18851276c101b819b9282ec1facdbfe761fcd
2byte <iambenf@gmail.com>
2016-12-13 17:41:18 +0800

    make change

--- hover.js
+++ hover.js
@@ -1,67 +1,109 @@
 (function(){
-	var x0,y0,x1,y1;
+	var xa,ya,xb,yb,x0,y0,x1,y1,x2,y2,k0,k1;
+	document.getElementById('test').onmouseenter=mouseEnterFunc;
 	
-	
-	document.getElementById('test').mouseenter=mouseEnter;
-	
-	function mouseEnter(e){
-		var e = e||window.event,k,
+	function mouseEnterFunc(e){
+		var e = e||window.event;
 		x1 = this.offsetLeft,
 		y1 = -this.offsetTop, //注意坐标，所有的y坐标都是负数
 		x2 = x1 + this.offsetWidth,
 		y2 = y1 - this.offsetHeight, //同样y坐标为负数
 		x0 = (x1 + x2) / 2,
-		y0 = (y1 + y2) / 2,xa,ya,xb,yb;
+		y0 = (y1 + y2) / 2,
+		k0 = (y2-y1)/(x2-x1);
 		
-		if(xa===undefined){
-			xa=getMousePos(e).x
-			ya=-getMousePos(e).y
-			console.log('第一次进入,',xa,ya,xb,yb)
-		}else{
-			if(x0!= getMousePos(e).x){
-			xb=getMousePos(e).x;
-			yb=-getMousePos(e).y;
-			console.log('第二次进入,',xa,ya);
-			k=(yb-ya)/(xb-xa)
-			console.log("k: ",k)
-			
-			setEnterCSS(this,k)
-			
-			document.getElementById('test').onmousemove=null;
+		xa=getMousePos(e).x;
+		ya=-getMousePos(e).y;
+		k= (ya-y0)/(xa-x0);
+		
+		var percentX=(xa-x0)/this.offsetWidth*100+'%',
+		percentY =(ya-y0)/this.offsetHeight*100+'%';
+		
+		
+		//console.log('当前坐标: ('+xa+','+ya+')')
+		//console.log('中心坐标: ',x0,y0)
+		//console.log('四角坐标: ',x1,y1,x2,y2)
+		//console.log(k,k0)
+		
+		
+		if(k>k0 && k<-k0){
+			if(xa>x0){
+				console.log('右侧')
+				setEnterCSS(this,2,percentY)
+			}
+			else{
+				console.log('左侧')
+				setEnterCSS(this,4,percentY)
 			}
 		}
-	}
+		else{
+			if(ya>y0){
+				console.log('顶侧')
+				setEnterCSS(this,1,percentX)
+			}
+			else{
+				console.log('底侧')
+				setEnterCSS(this,3,percentX)
+			}
+		}
 	
+			
+		}
 	
-	document.getElementById('test').mouseleave=function (event){
+	function mouseOverFunc(e){
+		console.log(xb,xa,getMousePos(e).x)
+		if(xb===undefined && xa!= getMousePos(e).x){
+			xb=getMousePos(e).x
+			yb=-getMousePos(e).y
+			console.log('坐标,',xa,ya,xb,yb)
+			k1=(yb-ya)/(xb-xa)
+			console.log("k: ",k1)
+			setEnterCSS(this,k)
+			document.getElementById('test').onmousemove=null;
+		}
+		
+		}
+
+	document.getElementById('test').onmouseleave=function (event){
 		var obj=this
-		document.getElementById('test').onmousemove=mouseEnter
 		console.log('mouse out')
-		x0=y0=x1=y1=undefined;
+		xa=xb=x2=y2=k0=x0=y0=x1=y1=undefined;
 		setTimeout(function(){
 			obj.classList.remove("test")
 		},0)
+		document.getElementById('test').onmouseenter=mouseEnterFunc
 	}
 	
 	
 	
-	function setEnterCSS(obj,k){
-		changeColor()
+	function setEnterCSS(obj,d,arg){
+		if(d==1){
+			changeColor(arg,'-100%')
+		}
+		else if(d==2){
+			changeColor('100%',arg)
+		}
+		else if(d==3){
+			changeColor(arg,'100%')
+		}
+		else{
+			changeColor('-100%',arg)
+		}
+		
 		setTimeout(function(){
 			obj.classList.add("test")
-		},100)
+		},300)
 		
-		console.log(obj,k)
 	}
 	
-	function changeColor () { // Flip psedo element's background color
+	function changeColor (x,y) { // Flip psedo element's background color
 				var beforeIndex = 0,  // ADDED: define varible and set it to the correct value to referrence the ::before psuedo element
 					position,
 					currentPosition,
 					styleSheetObject = document.getElementById("styleId"); // ADDED: get a refernce to the stylesheet object
 				currentPosition=styleSheetObject.sheet.cssRules[beforeIndex].style.transform;
 				
-				position ='10%,50%';
+				position =x+','+y;
 				newValue = 'translate3d('+position+',0)'
 				console.log(currentPosition)
 			     // Change the color -- Changed to directly reference the correct stylesheet via id
