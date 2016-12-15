98f92e525da832ef48b02b95d7089856982a84d6
2byte <iambenf@gmail.com>
2016-12-15 17:37:00 +0800

    alpha. job done

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Hover.js
 
-Hover.js is a tiny javascript plugin for hover effect.
+### 360 degree direction aware animationfor any elements
 
 ## Installation
 
@@ -16,4 +16,8 @@ Hover.js is a tiny javascript plugin for hover effect.
 
 maybe not very useful, i found this kind of effect on [hakim](http://hakim.se/)'s homepage. I want to make something same.
 
-After
+After trying 2-3 methods, I make this in an unefficient way. not very smart, but work.
+
+# to do
+
+still have a lot of things to fix. 
--- hover.js
+++ hover.js
@@ -1,16 +1,30 @@
 (function(){
 	var div = document.createElement("div"); 
 	div.style.backgroundColor='rgba(0,0,0,.3)'
-	div.style.width='10px';
-	div.style.height='10px';
+	div.style.width='0px';
+	div.style.height='0px';
 	div.style.position='absolute';
 	div.style.opacity='0';
-	div.style.visibility='hidden';
+	div.style.textAlign='center';
 	div.style.transition='width,height,opacity .15s,.15s,.15s';
+	div.style.transition='all .25s ease-in-out';
 	div.style.zIndex='-999';
+	div.style.color='#fff';
+	div.style.pointerEvents='none';
+	
 	document.body.appendChild(div);
 
-	document.onmousemove = function (e) {
+	
+	
+	list=document.querySelectorAll(".hoverJs")
+	for(var i in list){
+		list[i].onmouseenter=onHover;
+		list[i].onmouseleave=onLeave;
+	}
+	document.onmousemove = docuMove;
+	
+	
+	function docuMove(e){
 		var e = e || event,
 		scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft,
 		scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
@@ -18,29 +32,31 @@
 		div.style.left = e.clientX - parseInt(div.style.width)/2 + scrollLeft  + 'px';
 	};
 	
-	list=document.querySelectorAll(".hoverJs")
-	for(var i in list){
-		list[i].onmouseenter=onHover;
-		list[i].onmouseleave=onLeave;
-	}
-	
 	function onHover(e){
+		document.onmousemove=null;
+		var x1 = this.offsetLeft,
+			y1 = this.offsetTop;
+			
 		var e=e || event,
 		scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft,
 		scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		div.style.height = this.offsetHeight+'px';
+		div.style.lineHeight = div.style.height = this.offsetHeight+'px';
 		div.style.width = this.offsetWidth+'px';
 		div.style.opacity = 1;
-		div.style.visibility = 'visible';
-		div.style.display = 'block';
-		div.style.top = e.clientY  - this.offsetHeight/2 + scrollTop + 'px'; 
-		div.style.left = e.clientX  - this.offsetWidth/2 + scrollTop + 'px'; 
+		div.style.top = y1 + 'px'; 
+		div.style.left = x1 + 'px';
+		if(this.getAttribute('data-hoverJs')!=null) {
+		div.innerText = this.getAttribute('data-hoverJs');
+		div.style.zIndex = 999;
+		}
 	}
 	function onLeave(e){
 		var scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft,
 		scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
 		div.style.opacity = 0;
-		div.style.visibility='hidden';
+		div.innerText = null;
+		div.style.zIndex = -999;
+		document.onmousemove = docuMove;
 	}
 
 })();
